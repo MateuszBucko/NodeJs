3c19211c788c737b150a44c32ec6439d1a5d6435
Mateusz Bucko <mateuszbucko1@gmail.com>
2017-01-21 01:43:49 +0100

    Poprawki do dodawania ksiazek itp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- var QUERY_URL = 'http://localhost:8080/';
+var QUERY_URL = 'http://localhost:8080/';
 var view = 0;
 var books;
 var authors;
@@ -14,20 +14,20 @@ var c_DESCRIPTION = 7;
 var c_ISBN = 8;
 var c_DATE = 9;
 
-function CompareString(a, b){
+function CompareString(a, b) {
     return a.localeCompare(b);
 }
 
-function Author(id, name, surname){
+function Author(id, name, surname) {
     this.id = ko.observable(id);
     this.name = ko.observable(name);
     this.surname = ko.observable(surname);
 }
 
-function header(name,sort_desc,sort_asc,parent){
-    this.name=name;
-    this.sort_desc=parent.sort(sort_desc);
-    this.sort_asc=parent.sort(sort_asc);
+function header(name, sort_desc, sort_asc, parent) {
+    this.name = name;
+    this.sort_desc = parent.sort(sort_desc);
+    this.sort_asc = parent.sort(sort_asc);
 }
 
 
@@ -35,22 +35,22 @@ function AuthorsViewModel() {
     var self = this;
     this.authors = ko.observableArray();
     /*
-    this.headers = ko.observableArray([
-        new header('id',),
-    ]);*/
+     this.headers = ko.observableArray([
+     new header('id',),
+     ]);*/
     self.nameIn = ko.observable("");
     self.surnameIn = ko.observable("");
     $.getJSON(QUERY_URL + 'authors', {}, function (data) {
-        var mappedData = $.map(data, function(item){
+        var mappedData = $.map(data, function (item) {
             return new Author(item.id,
-                item.name,
-                item.surname);
+                    item.name,
+                    item.surname);
         });
         self.authors(mappedData);
     });
     this.add = function () {
         console.log(this);
-      //self.authors.push(new Authors(this.name, this.surname));  
+        //self.authors.push(new Authors(this.name, this.surname));  
     };
     this.removeAuthor = function (author) {
         console.log(author.id());
@@ -64,43 +64,81 @@ function AuthorsViewModel() {
         });
     };
     this.sortASC = function (column) {
-        self.authors.sort(function(a, b){
+        self.authors.sort(function (a, b) {
             var z, x;
-            switch(column){
-                case 'id' : return a-b; break;
-                case 'name' : z=a.name(); x=b.name(); break;
-                case 'surname' : z=a.surname(); x=b.surname();; break;
+            switch (column) {
+                case 'id' :
+                    return a - b;
+                    break;
+                case 'name' :
+                    z = a.name();
+                    x = b.name();
+                    break;
+                case 'surname' :
+                    z = a.surname();
+                    x = b.surname();
+                    ;
+                    break;
             }
             return z.localeCompare(x);
         });
     };
-    this.nextid = ko.computed(function (){
-        return self.authors().length+1;
-    },this);
-    this.add = function(){
-        var newauthor = new Author(15,this.nameIn(),this.surnameIn());
-        $.ajax({
-            url: QUERY_URL + 'authors/',
-            type: 'post',
-            data: ko.toJSON(newauthor),
-            contentType: "application/json",
-            success: function (result) {
-                authors.push(new Author(result.id,result.name,result.surname));
-            }
+    this.nextid = ko.computed(function () {
+        return self.authors().length + 1;
+    }, this);
+
+    this.add = function () {
+        var newauthor = new Author(15, this.nameIn(), this.surnameIn());
+        if (this.nameIn() !== null && this.nameIn().length > 0 && this.surnameIn() !== null && this.surnameIn().length > 0) {
+            $.ajax({
+                url: QUERY_URL + 'authors/',
+                type: 'post',
+                data: ko.toJSON(newauthor),
+                contentType: "application/json",
+                success: function (result) {
+                    authors.push(new Author(result.id, result.name, result.surname));
+                }
+            });
+        } else {
+            alert("Imię i nazwisko nie może być puste !")
+        }
+    };
+
+    this.sortIdDesc = function () {
+        self.authors.sort(function (a, b) {
+            return a.id() - b.id();
         });
     };
-    
-    this.sortIdDesc = function () {self.authors.sort(function(a, b) {return a.id()-b.id();});};
-    this.sortNameDesc = function () {self.authors.sort(function(a, b) {return CompareString(a.name(),b.name());});};
-    this.sortSurnameDesc = function () {self.authors.sort(function(a, b) {return CompareString(a.surname(),b.surname());});};
-    
-    this.sortIdAsc = function () {self.authors.sort(function(a, b) {return b.id()-a.id();});};
-    this.sortNameAsc = function () {self.authors.sort(function(a, b) {return CompareString(b.name(),a.name());});};
-    this.sortSurnameAsc = function () {self.authors.sort(function(a, b) {return CompareString(b.surname(),a.surname());});};
-    
+    this.sortNameDesc = function () {
+        self.authors.sort(function (a, b) {
+            return CompareString(a.name(), b.name());
+        });
+    };
+    this.sortSurnameDesc = function () {
+        self.authors.sort(function (a, b) {
+            return CompareString(a.surname(), b.surname());
+        });
+    };
+
+    this.sortIdAsc = function () {
+        self.authors.sort(function (a, b) {
+            return b.id() - a.id();
+        });
+    };
+    this.sortNameAsc = function () {
+        self.authors.sort(function (a, b) {
+            return CompareString(b.name(), a.name());
+        });
+    };
+    this.sortSurnameAsc = function () {
+        self.authors.sort(function (a, b) {
+            return CompareString(b.surname(), a.surname());
+        });
+    };
+
 }
 
-function Book(id, title, title_en, isbn, add_date, category, description, publisher){
+function Book(id, title, title_en, isbn, add_date, category, description, publisher) {
     this.id = ko.observable(id);
     this.title = ko.observable(title);
     this.title_en = ko.observable(title_en);
@@ -122,20 +160,20 @@ function BooksViewModel() {
     this.description = ko.observable("");
     this.publisher = ko.observable("");
     $.getJSON(QUERY_URL + 'books', {}, function (data) {
-        var mappedData = $.map(data, function(item){
+        var mappedData = $.map(data, function (item) {
             return new Book(item.id,
-                item.title,
-                item.title_en,
-                item.isbn,
-                item.add_date,
-                item.category,
-                item.description,
-                item.publisher);
+                    item.title,
+                    item.title_en,
+                    item.isbn,
+                    item.add_date,
+                    item.category,
+                    item.description,
+                    item.publisher);
         });
         self.books(mappedData);
     });
-    this.add = function(){
-        var newbook = new Book(15,this.title(),
+    this.add = function () {
+        var newbook = new Book(15, this.title(),
                 this.title_en(),
                 this.isbn(),
                 this.add_date(),
@@ -143,18 +181,19 @@ function BooksViewModel() {
                 this.description(),
                 this.publisher());
         $.ajax({
-            url: QUERY_URL + 'authors/',
+            url: QUERY_URL + 'books/',
             type: 'post',
-            data: ko.toJSON(newauthor),
+            data: ko.toJSON(newbook),
             contentType: "application/json",
             success: function (result) {
-                authors.push(new Author(result.id,result.name,result.surname));
+                books.push(new Book(result.id, result.title, result.title_en, result.isbn, 
+                result.add_date, result.category, result.description, result.publisher));
             }
         });
     };
-    this.nextid = ko.computed(function (){
-        return self.books().length+1;
-    },this);
+    this.nextid = ko.computed(function () {
+        return self.books().length + 1;
+    }, this);
     this.removeBook = function (book) {
         $.ajax({
             url: QUERY_URL + 'books/' + book.id(),
@@ -165,23 +204,50 @@ function BooksViewModel() {
             }
         });
     };
-    this.sort = function(column,desc) {
-        self.books.sort(function(a, b){
+    this.sort = function (column, desc) {
+        self.books.sort(function (a, b) {
             var z, x;
-            switch(column){
-                case c_ID : if(desc)return b.id()-a.id();
-                            else return a.id()-b.id(); break;
-                case c_TITLE : z=a.title(); x=b.title(); break;
-                case c_TITLE_EN : z=a.title_en(); x=b.title_en(); break;
-                case c_CATEGORY : z=a.category(); x=b.category(); break;
-                case c_PUBLISHER : z=a.publisher(); x=b.publisher(); break;
-                case c_DESCRIPTION : z=a.description(); x=b.description(); break;
-                case c_ISBN : if(desc)return b.isbn()-a.isbn();
-                            else return a.isbn()-b.isbn(); break;
-                case c_DATE : if(desc)return b.add_date()-a.add_date();
-                            else return a.add_date()-b.add_date(); break;
+            switch (column) {
+                case c_ID :
+                    if (desc)
+                        return b.id() - a.id();
+                    else
+                        return a.id() - b.id();
+                    break;
+                case c_TITLE :
+                    z = a.title();
+                    x = b.title();
+                    break;
+                case c_TITLE_EN :
+                    z = a.title_en();
+                    x = b.title_en();
+                    break;
+                case c_CATEGORY :
+                    z = a.category();
+                    x = b.category();
+                    break;
+                case c_PUBLISHER :
+                    z = a.publisher();
+                    x = b.publisher();
+                    break;
+                case c_DESCRIPTION :
+                    z = a.description();
+                    x = b.description();
+                    break;
+                case c_ISBN :
+                    if (desc)
+                        return b.isbn() - a.isbn();
+                    else
+                        return a.isbn() - b.isbn();
+                    break;
+                case c_DATE :
+                    if (desc)
+                        return b.add_date() - a.add_date();
+                    else
+                        return a.add_date() - b.add_date();
+                    break;
             }
-            if(desc)
+            if (desc)
                 return x.localeCompare(z);
             else
                 return z.localeCompare(x);
@@ -189,40 +255,40 @@ function BooksViewModel() {
     };
 }
 
-function View(title, template, data){
-    this.title=title;
-    this.template=template;
-    this.data=data;
+function View(title, template, data) {
+    this.title = title;
+    this.template = template;
+    this.data = data;
 }
 
 function ViewsModel() {
     var self = this;
-    
+
     this.views = ko.observableArray([
-       new View('books','books_template',BooksViewModel),
-       new View('authors','authors_template',AuthorsViewModel)
+        new View('books', 'books_template', BooksViewModel),
+        new View('authors', 'authors_template', AuthorsViewModel)
     ]);
     this.selectedView = ko.observable();
-    this.sort = function(a,b){
-       console.log(self.selectedView().data);
+    this.sort = function (a, b) {
+        console.log(self.selectedView().data);
     }
-    this.add = function(){
+    this.add = function () {
         console.log('lol');
     }
 }
 var active;
 
 $(document).ready(function () {
-   ko.applyBindings(new ViewsModel());
-   
-   active = $('#navigation_bar a:first-child').addClass('active');
-   $("#navigation_bar a.active").click();
-   $("#navigation_bar a").click(function(e){
-       $(active).removeClass('active');
-       active = $(this).addClass('active');
-   });
-   $(".sort").click(function(e){
-      e.preventDefault(); 
-   });
+    ko.applyBindings(new ViewsModel());
+
+    active = $('#navigation_bar a:first-child').addClass('active');
+    $("#navigation_bar a.active").click();
+    $("#navigation_bar a").click(function (e) {
+        $(active).removeClass('active');
+        active = $(this).addClass('active');
+    });
+    $(".sort").click(function (e) {
+        e.preventDefault();
+    });
 
 });
